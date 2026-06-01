f5749e663b8bdb4177aa1140ec508209746b04af
upthebrand <gary@upthebrand.com>
2026-06-01 16:44:43 -0400

    Financing

---
+++ src/data/financing.ts
@@ -0,0 +1,9 @@
+/** Sage’s co-branded Enhancify application (opens in a new tab). */
+export const financingApplyUrl = 'https://www.enhancify.com/sage-home-improvements';
+
+export const financingAnchor = '#financing';
+
+/** Use on pages other than the homepage (e.g. thank-you). */
+export const financingHomeHref = '/#financing';
+
+export const financingPartnerName = 'Enhancify';
